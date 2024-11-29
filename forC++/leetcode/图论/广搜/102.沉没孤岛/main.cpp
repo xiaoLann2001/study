47451d820fdf700d0596d63339b1bced64e9722d
xiaolan <songguoqing2001@163.com>
2024-11-29 07:39:08 +0000

    241129#leetcode

---
+++ forC++/leetcode/图论/广搜/102.沉没孤岛/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 四个方向
+const vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+// 广度优先搜索
+void bfs(vector<vector<int>> &map, vector<vector<int>> &map_copy, int x, int y) {
+    queue<pair<int, int>> queue_next;
+    queue_next.push(pair<int, int>(x, y));
+    map[x][y] = 0;  // 加入队列立即标记
+    while (!queue_next.empty()) {
+        // 当前格子
+        int x_cur = queue_next.front().first;
+        int y_cur = queue_next.front().second;
+        queue_next.pop();
+        map_copy[x_cur][y_cur] = 1; // 在新的矩阵中生成原图的非孤岛
+        // 相邻格子
+        for (auto v : dir) {
+            int x_next = x_cur + v.first;
+            int y_next = y_cur + v.second;
+            if (x_next < 0 || y_next < 0 || // 越界
+                x_next > map.size() - 1 || y_next > map[0].size() - 1) continue;
+            if (map[x_next][y_next] == 1) {
+                queue_next.push(pair<int, int>(x_next, y_next));
+                map[x_next][y_next] = 0;  // 加入队列立即标记
+            }
+        }
+    }
+}
+
+int main() {
+    // 输入并初始化
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> map(n, vector<int>(m));
+    vector<vector<int>> map_copy(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    // 采用广度优先搜索
+    // 遍历左右两侧的边缘
+    for (int i = 0; i < map.size(); i++) {
+        if (map[i][0] == 1) bfs(map, map_copy, i, 0);
+        if (map[i][m - 1] == 1) bfs(map, map_copy, i, m - 1);
+    }
+
+    // 遍历上下两侧的边缘
+    for (int j = 0; j < map[0].size(); j++) {
+        if (map[0][j] == 1) bfs(map, map_copy, 0, j);
+        if (map[n - 1][j] == 1) bfs(map, map_copy, n - 1, j);
+    }
+
+    // 输出将孤岛“沉没”之后的岛屿矩阵。 注意：每个元素后面都有一个空格
+    for (auto v : map_copy) {
+        for (auto n : v) cout << n << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
